53257e8dfd9cbbbad01405da969e8e9d0031faa8
Vjacheslav Borisov <slavb@iconicompany.com>
2023-03-31 12:00:40 +0400

    fix test

--- test/mailer.test.js
+++ test/mailer.test.js
@@ -4,15 +4,15 @@ const ifsetMailUrl = process.env.MAIL_URL ? describe : describe.skip;
 ifsetMailUrl('MAIL_URL test', () => {
   test('MAIL_URL sendMail', async () => {
     if (process.env.SERVER_ADMIN) {
-        const message =  {
-            from: process.env.MAIL_FROM,
-            to: process.env.SERVER_ADMIN,
-            subject: 'TEST SUBJECT',
-            text: 'TEST BODY',
-          };
-        message.to = process.env.SERVER_ADMIN;
-        await mailer.sendMail(message);
-        expect(1).toStrictEqual(1);
-}});
-
+      const message = {
+        from: process.env.MAIL_FROM,
+        to: process.env.SERVER_ADMIN,
+        subject: 'TEST SUBJECT',
+        text: 'TEST BODY'
+      };
+      message.to = process.env.SERVER_ADMIN;
+      const res = await mailer.sendMail(message);
+      expect(res.accepted).toStrictEqual([process.env.SERVER_ADMIN]);
+    }
+  });
 });
