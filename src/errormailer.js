de031431749c65d9dfe8dbb767c7e8323bc22e0d
Vjacheslav Borisov <slavb@iconicompany.com>
2023-03-31 12:09:50 +0400

    add MAIL_FROM

--- src/errormailer.js
+++ src/errormailer.js
@@ -3,13 +3,16 @@ const { mailer } = require('./mailer');
 function format(err) {
   return {
     subject: `[NODEJS FATAL ERROR]`,
-    text: `${err.message}\n${err.addMessage||''}\n${err.stack}`
+    text: `${err.message}\n${err.addMessage || ''}\n${err.stack}`
   };
 }
 async function notify(err) {
   if (process.env.SERVER_ADMIN) {
     const message = format(err);
     message.to = process.env.SERVER_ADMIN;
+    if (process.env.MAIL_FROM) {
+      message.from = process.env.MAIL_FROM;
+    }
     await mailer.sendMail(message);
   } else {
     console.log(err);
